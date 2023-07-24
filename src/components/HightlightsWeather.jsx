246fbf063a9b31d39972775d063f1744407b1773
TJIGRIS <johanalvarezz237@gmail.com>
2023-07-24 15:32:20 -0500

    terminado el main, ahora sigue hacer funcionalidades

--- src/components/HightlightsWeather.jsx
+++ src/components/HightlightsWeather.jsx
@@ -2,14 +2,14 @@ import { LiaLocationArrowSolid } from 'react-icons/lia'
 
 export function HightlightsWeather () {
   return (
-    <section className='mt-[72px] '>
+    <section className=''>
       <h2 className='text-[24px] font-bold'>Today´s Highlights</h2>
 
       <div className='grid grid-cols-layout-alt gap-[48px] mt-[32px]'>
         <article className='flex flex-col items-center justify-between p-[16px] bg-primary'>
           <h3>Wind status</h3>
 
-          <div>
+          <div className='text-[64px] my-[12px]'>
             7<span>mph</span>
           </div>
 
@@ -24,18 +24,18 @@ export function HightlightsWeather () {
         <article className='flex flex-col items-center justify-between p-[16px] bg-primary'>
           <h3>Humidity</h3>
 
-          <div>
+          <div className='text-[64px] my-[13px]'>
             84<span>%</span>
           </div>
 
-          <div className='flex justify-between w-[90px]'>
-            <div>
+          <div className='flex flex-col justify-between w-[320px] text-text-alt'>
+            <div className='flex justify-between'>
               <p>0</p>
               <p>50</p>
               <p>100</p>
             </div>
-            <input type='range' />
-            <p>%</p>
+            <input type='range' className='w-full' />
+            <p className='ml-auto'>%</p>
           </div>
         </article>
 
